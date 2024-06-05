ede08d98de10960d9a35c8a68e2f61fde8ae972a
honeydreamchaser <138431748+honeydreamchaser@users.noreply.github.com>
2024-06-05 16:30:09 -0400

    asdf

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import uvicorn
-import app.user.model as userModel
-import app.email_verify.model as emailverifyModel
-from database import engine
 
-# userModel.Base.metadata.create_all(bind=engine)
-# emailverifyModel.Base.metadata.create_all(bind=engine)
+from app.api import app as qbdr_app
+
+app = qbdr_app
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", 
-                host="0.0.0.0", 
-                port=8000, 
-                reload=False)
+    uvicorn.run(app)
