32c29c37aad9e5d25b498cdac68d61cddbc20e0b
Luffy <luffy0105@gmail.com>
2024-06-05 02:38:24 +0200

    remove mysql environments

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from decouple import config
 
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = config("POSTGRES_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
