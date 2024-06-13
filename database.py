ba70fdf68a0f0f80e7134cfb5df4f479c35b1403
honeydreamchaser <138431748+honeydreamchaser@users.noreply.github.com>
2024-06-13 10:00:52 -0400

    parse job data

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ SQLALCHEMY_DATABASE_URL = config("POSTGRESQL_URL")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     echo=False,
-    pool_recycle=3600
+    pool_size=20,
+    max_overflow=0
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
